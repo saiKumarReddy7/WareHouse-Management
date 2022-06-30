e2477371ea9837cea05784f3082d7d53dc9db677
saiKumarReddy7 <98637529+saiKumarReddy7@users.noreply.github.com>
2022-06-30 22:12:58 +0530

    Update and rename README.md to Menu.java

--- Menu.java
+++ Menu.java
@@ -1,10 +1,10 @@
 # WareHouse-Management
 #In this project we are going to store the list of computers, and we are going to do various operations using Java
 package Main;
+
 import java.util.Scanner;
 public class Menu {
     public void menu( Shelf shelf ) {
-
         int value;
         do {
             Scanner input = new Scanner(System.in);
